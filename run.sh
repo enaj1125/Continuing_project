394e60692a937f73b457037830fe94c192b95ef8
Yan Jiang <enaj1125@gmail.com>
2016-07-11 21:20:40 -0700

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-# example of the run script for running the average_degree calculation
+
 
 # I'll execute my programs, with the input directory tweet_input and output the files in the directory tweet_output
 python ./src/Final_find_median.py ./tweet_input/tweets.txt ./tweet_output/output.txt
