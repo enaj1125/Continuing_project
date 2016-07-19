04fb8388b3eb45d52c664b793d62a036d4f5113f
Yan Jiang <enaj1125@gmail.com>
2016-07-19 13:09:28 -0700

    Update rolling_median.py

--- rolling_median.py
+++ rolling_median.py
@@ -1,4 +1,4 @@
-# maintain two data structure: heap of stream data and heap of current edges
+# The goal of this program is to 1) Use Venmo payments that stream in to build a graph of users and their relationship with one another. 2) Calculate the median degree of a vertex in a graph and update this each time a new Venmo payment appears. Will be calculating the median degree across a 60-second sliding window.
 
 try:                                                     # use json or the simplejson as a fallback.
     import json
